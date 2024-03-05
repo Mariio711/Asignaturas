cec168bed1419418491de1d598b04ec2a1ad2795
Mariio711 <80849476+Mariio711@users.noreply.github.com>
2024-03-05 02:48:09 +0100

    avances poo

---
+++ POO/Escobar_Vidal_Mario/P0/cadena.hpp
@@ -0,0 +1,12 @@
+#ifndef CADENA
+#define CADENA
+class Cadena{
+    public:
+        
+    private:
+        
+        //atributos
+        char* cadena; // puntero a una cadena de caracteres
+        size_t tamano; // tamaño de la cadena
+};
+#endif
