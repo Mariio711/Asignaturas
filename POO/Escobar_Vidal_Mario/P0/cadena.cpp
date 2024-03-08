9ee6dc98ce997dda5e5894c68a93198152fe2aea
Mariio711 <80849476+Mariio711@users.noreply.github.com>
2024-03-08 16:58:26 +0100

    Clase cadena terminada sin errores aparentes

--- POO/Escobar_Vidal_Mario/P0/cadena.cpp
+++ POO/Escobar_Vidal_Mario/P0/cadena.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+char Cadena::vacio[1] = {'\0'}; // Inicialización del caracter terminador de la cadena
+
 //constructor con dos parametros
 Cadena::Cadena(int tam, char s){
     if(tam == 0){
@@ -25,6 +27,13 @@ Cadena::Cadena(const char* s){
     s_ = new char[strlen(s)+1]; //asignamos memoria dinamica del tamaño de la cadena dada mas un espacio para el caracter termiandor
     strcpy(s_, s); //copiamos la cadena de s en s_ 
     tam_ = strlen(s_); //asignamos el tamaño a la instancia de la cadena
+}
+
+//constructor de copia
+Cadena::Cadena(const Cadena& otra){
+    tam_ = otra.tam_;
+    s_ = new char(tam_ + 1);
+    strcpy(s_, otra.s_);
 }
 
 //sobrecarga de operador de asignacion (=) para una cadena
@@ -74,10 +83,136 @@ Cadena& Cadena::operator=(const char* s){
     return *this;
 }  
 
-explicit Cadena::operator const char *() const{
+Cadena::operator const char *() const{
     return (tam_ > 0) ? s_ : vacio;
 }
 
-int Cadena::length(){
+
+
+int Cadena::length()const{
     return tam_;
+}
+
+//sobrecarga del operador de suma con asignacion
+Cadena& Cadena::operator+=(const Cadena& otra){  
+    int nueva_longitud = tam_ + otra.tam_ + 1;
+    char* nueva_s = nullptr;
+
+    //si no hay suficiente memoria no se realiza la asignacion
+    try{
+        nueva_s = new char[nueva_longitud]; // asignamos memoria
+    }catch(const std::bad_alloc&){
+        return *this;
+    }
+
+    //si la asignacion de memoria fue exitosa
+    strcpy(nueva_s, s_);
+    strcat(nueva_s, otra.s_);
+
+    //liberamos la memoria antigua y actualizamos los miembros
+    delete[] s_;
+    s_ = nueva_s;
+    tam_ = strlen(strcat(s_, otra.s_)) + 1;
+    return *this;
+}
+
+//sobrecarga del operador de suma
+Cadena Cadena::operator+(const Cadena& otra){
+    int nueva_longitud = tam_ + otra.tam_ + 1;
+    char* nueva_s = nullptr;
+
+    //si no hay suficiente memoria no se realiza la asignacion
+    try{
+        nueva_s = new char[nueva_longitud]; // asignamos memoria
+    }catch(const std::bad_alloc&){
+        throw; // Lanzamos la excepción hacia arriba
+    }
+
+    //si la asignacion de memoria fue exitosa
+    strcpy(nueva_s, s_);
+    strcat(nueva_s, otra.s_);
+
+    // Creamos una nueva Cadena con la cadena concatenada
+    Cadena nueva_cadena(nueva_longitud - 1);
+    strcpy(nueva_cadena.s_, nueva_s);
+
+    // Liberamos la memoria temporal
+    delete[] nueva_s;
+
+    return nueva_cadena;
+}
+
+//sobrecarga del operador de igualdad
+bool Cadena::operator ==(const Cadena& otra) const{
+    return strcmp(s_, otra.s_) == 0;
+}
+
+//sobrecarga del operador de igualdad (cadena primero)
+bool operator ==(const char* str, const Cadena& cadena){
+    return cadena == str;
+}
+
+//sobrecarga del operador distinto
+bool Cadena::operator !=(const Cadena& otra) const{
+    return strcmp(s_, otra.s_) != 0;
+}
+
+//sobrecarga del operador menor que
+bool Cadena::operator <(const Cadena& otra) const{
+    return strcmp(s_, otra.s_) < 0;
+}
+
+//sobrecarga del operador mayor que
+bool Cadena::operator >(const Cadena& otra) const{
+    return strcmp(s_, otra.s_) > 0;
+}
+
+//sobrecarga del operador menor igual
+bool Cadena::operator <=(const Cadena& otra) const{
+    return strcmp(s_, otra.s_) <= 0;
+}
+
+//sobrecarga del operador mayor igual
+bool Cadena::operator >=(const Cadena& otra) const{
+    return strcmp(s_, otra.s_) >= 0;
+}
+
+//sobrecarga del operador de indice
+#include <stdexcept> // para std::out_of_range
+
+char& Cadena::operator[](int unsigned indice) {
+    if (indice >= tam_) {
+        throw std::out_of_range("Índice fuera de rango");
+    }
+    return s_[indice];
+}
+
+const char& Cadena::operator[](int unsigned indice) const {
+    if (indice >= tam_) {
+        throw std::out_of_range("Índice fuera de rango");
+    }
+    return s_[indice];
+}
+
+// devuelve el caracter al que apunta la posicion del indice
+char& Cadena::at(int unsigned indice){
+    if (indice >= tam_) {
+        throw std::out_of_range("Índice fuera de rango");
+    }
+    return s_[indice];
+}
+
+Cadena Cadena::substr(unsigned int indice, unsigned int tam){
+    if (indice >= tam_ || tam > tam_ - indice) {
+        throw std::out_of_range("Índice fuera de rango");
+    }
+    
+    char* subcadena = new char[tam + 1];
+    strncpy(subcadena, s_ + indice, tam);
+    subcadena[tam] = '\0'; // Asegúrate de que la subcadena esté terminada en null
+
+    Cadena resultado(subcadena);
+    delete[] subcadena; // No olvides liberar la memoria que has asignado con new
+
+    return resultado;
 }
--- POO/Escobar_Vidal_Mario/P0/cadena.hpp
+++ POO/Escobar_Vidal_Mario/P0/cadena.hpp
@@ -1,21 +1,42 @@
 #ifndef CADENA
 #define CADENA
+#include <cstddef>
+
 class Cadena{
     public:
 
         //constructores
         Cadena(int tam = 0, char s = ' '); //con dos parametros, los demas se consctruiran a partir de este
         Cadena(const char* s); //a partir de una cadena de bajo nivel
+        Cadena(const Cadena& otra); //constructor de copia
 
         //sobrecarga de operadores
         Cadena& operator=(const Cadena& otra); //sobrecarga del operador de asignacion para otra clase cadena
         Cadena& operator=(const char* s); //sobrecarga del operador de asignacion para otra clase cadena
+        Cadena& operator+=(const Cadena& otra);//sobrecarga del operador de suma con asignacion
+        Cadena operator+(const Cadena& otra);//sobrecarga del operador de suma
         
         //sobrecarga de operadores de conversion
         explicit operator const char*() const;
 
+        //sobrecarga de operadores logicos
+        bool operator ==(const Cadena& otra) const;
+        bool operator !=(const Cadena& otra) const;
+        bool operator <(const Cadena& otra) const;
+        bool operator >(const Cadena& otra) const;
+        bool operator <=(const Cadena& otra) const;
+        bool operator >=(const Cadena& otra) const;
+
+        //sobrecarga de operador de indice
+        char& operator[](int unsigned indice); // devuelve el caracter al que apunta la posicion del indice
+        const char& operator[](int unsigned indice) const; // devuelve el caracter al que apunta la posicion del indice
+        char& at(int unsigned indice); // devuelve el caracter al que apunta la posicion del indice
+        
         //metodos observadores
-        int length();
+        int length() const; //devuelve el tamaño de la cadena
+
+        Cadena substr(int unsigned indice, int unsigned tam); //devuelve un substring de la cadena conla que se llama a partir del indice y el tamaño
+        
     private:
         
         //atributos
@@ -25,5 +46,5 @@ class Cadena{
 
 };
 
-char Cadena::vacio[1] = {'\0'}; // Inicialización del caracter terminador de la cadena
+bool operator ==(const char* str, const Cadena& cadena);
 #endif 
