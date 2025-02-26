9f567e662ae3c9dace44d1f902e750c5912c2be8
Mariio711 <marioevpacheco@gmail.com>
2025-02-26 12:29:46 +0100

    Primera sesion poo

---
+++ POO/Escobar_Vidal_Mario/P0/Fecha.cpp
@@ -0,0 +1,17 @@
+#include <ctime>
+#include "Fecha.hpp"
+
+//Constructor 
+Fecha::Fecha(int d, int m, int a): dia_{d}, mes_{m}, anno_{a} {
+    std::time_t tiempo_calendario = std::time(nullptr);
+    std::tm* tiempo_descompuesto = std::localtime(&tiempo_calendario);
+    int dia = tiempo_descompuesto->tm_mday;
+    int mes = tiempo_descompuesto->tm_mon + 1; // tm_mon: 0 (ene)..11 (dic)
+    int anno = tiempo_descompuesto->tm_year + 1900; // tm_year: a~nos desde 1900 
+    
+    if (d == 0){dia_ = dia;};
+    if (m == 0){mes_ = mes;};
+    if (a == 0){anno_ = anno;};
+
+    //valida(); // funcion que comprueba que la fecha es correcta
+}
---
+++ POO/Escobar_Vidal_Mario/P0/Fecha.hpp
@@ -0,0 +1,22 @@
+#ifndef FECHA_H_
+#define FECHA_H_
+
+class Fecha{
+    public:
+        //Metodos observadores
+        int dia()const {return dia_;};
+        int mes()const {return mes_;};
+        int anno()const {return anno_;};
+
+        //Constructores
+        Fecha(int d = 0, int m = 0, int a = 0); //con tres parámetros
+
+
+    private:
+        int dia_;
+        int mes_;
+        int anno_;
+
+};
+
+#endif
