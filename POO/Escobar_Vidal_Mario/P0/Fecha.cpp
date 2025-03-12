d8bbef20c03e11c4f41c7d3a6c1d4de2ac608448
Mariio711 <marioevpacheco@gmail.com>
2025-03-12 14:03:31 +0100

    luego termino

--- POO/Escobar_Vidal_Mario/P0/Fecha.cpp
+++ POO/Escobar_Vidal_Mario/P0/Fecha.cpp
@@ -1,17 +1,53 @@
-#include <ctime>
 #include "Fecha.hpp"
+#include <ctime>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+#include <sstream>
 
-//Constructor 
-Fecha::Fecha(int d, int m, int a): dia_{d}, mes_{m}, anno_{a} {
+const int dias_mes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+void Fecha::obtener_fecha_actual(int &dia, int &mes, int &anno){
     std::time_t tiempo_calendario = std::time(nullptr);
     std::tm* tiempo_descompuesto = std::localtime(&tiempo_calendario);
-    int dia = tiempo_descompuesto->tm_mday;
-    int mes = tiempo_descompuesto->tm_mon + 1; // tm_mon: 0 (ene)..11 (dic)
-    int anno = tiempo_descompuesto->tm_year + 1900; // tm_year: a~nos desde 1900 
+    dia = tiempo_descompuesto->tm_mday;
+    mes = tiempo_descompuesto->tm_mon + 1; // tm_mon: 0 (ene)..11 (dic)
+    anno = tiempo_descompuesto->tm_year + 1900; // tm_year: a~nos desde 1900 
+}
+
+//Funcion que valida si una fecha es valida
+bool Fecha::valida() const{
+    if(mes_ < 1 || mes_ > 12){return false;};
+    int dias_en_mes = dias_mes[mes_-1];
+
+    //comprobamos si es año bisiesto
+    if (mes_ == 2 && ((anno_ % 4 == 0 && anno_ % 100 != 0) || (anno_ % 400 == 0))) {
+        dias_en_mes = 29;
+    }
+
+    return dia_ >= 1 && dia_ <= dias_en_mes;
+}
+
+//Constructor por defecto
+Fecha::Fecha(int d, int m, int a): dia_{d}, mes_{m}, anno_{a} {
+    int dia, mes, anno;
+    obtener_fecha_actual(dia, mes, anno);
     
     if (d == 0){dia_ = dia;};
     if (m == 0){mes_ = mes;};
     if (a == 0){anno_ = anno;};
  
-    //valida(); // funcion que comprueba que la fecha es correcta
+    if (!valida()) {
+        throw std::invalid_argument("Fecha inválida");
+    }
+}
+
+// Constructor que toma un char*
+Fecha::Fecha(const char* f) {
+    int d, m, a;
+    
+
+    if (!valida()) {
+        throw std::invalid_argument("Fecha inválida");
+    }
 }
--- POO/Escobar_Vidal_Mario/P0/Fecha.hpp
+++ POO/Escobar_Vidal_Mario/P0/Fecha.hpp
@@ -3,6 +3,9 @@
 
 class Fecha{
     public:
+        //constantes año max y año min
+        const int AnnoMininmo = 1902, AnnoMaximo = 2037;
+
         //Metodos observadores
         int dia()const {return dia_;};
         int mes()const {return mes_;};
@@ -10,12 +13,14 @@ class Fecha{
 
         //Constructores
         Fecha(int d = 0, int m = 0, int a = 0); //con tres parámetros
+        Fecha(const Fecha& f): dia_{f.dia_}, mes_{f.mes_}, anno_{f.anno_}{} // de copia
+        Fecha(const char * f);
 
 
     private:
-        int dia_;
-        int mes_;
-        int anno_;
+        int dia_, mes_, anno_;
+        bool valida() const;
+        void obtener_fecha_actual(int &dia, int &mes, int &anno);
 
 };
  
