7f596a884128574fb5404b980364c3b8ef9a1761
Mariio711 <80849476+Mariio711@users.noreply.github.com>
2025-03-20 14:28:02 +0100

    cadena a medias, falta poco

--- POO/Escobar_Vidal_Mario/P0/Cadena.cpp
+++ POO/Escobar_Vidal_Mario/P0/Cadena.cpp
@@ -1,7 +1,100 @@
 #include "cadena.hpp"
+#include <stdexcept>
 
 //constructor predeterminado
-Cadena::Cadena(int n, char a):tam_(n), cadena_(new char[n + 1]){
-    std::memset(cadena_, a, n);
-    cadena_[n] = '\0';
+Cadena::Cadena(int n, char a):tam_(n), s_(new char[n + 1]){
+    std::memset(s_, a, n);
+    s_[n] = '\0';
 };
+
+//constructor de conversión
+Cadena::Cadena(const char* cadena) : tam_(std::strlen(cadena)), s_(new char[tam_ + 1]) {
+    std::strcpy(s_, cadena);
+}
+
+//constructor de copia
+Cadena::Cadena(const Cadena& otra): Cadena(otra.s_){}       //delegamos la copia en la conversion
+
+//operador de asignacion
+Cadena& Cadena::operator=(const Cadena& otra) {
+    if (this != &otra) {
+        delete[] s_; // Liberar la memoria existente
+        tam_ = otra.tam_;
+        s_ = new char[tam_ + 1];
+        std::strcpy(s_, otra.s_);
+    }
+    return *this;
+}
+
+//destructor
+Cadena::~Cadena(){
+    delete[] s_;
+}
+
+//operadores de comparacion
+bool operator==(const Cadena& a, const Cadena& b){
+    return std::strcmp(a.s_, b.s_) == 0;
+}
+
+bool operator<(const Cadena& a, const Cadena& b) {
+    return std::strcmp(a.s_, b.s_) < 0;
+}
+
+bool operator>(const Cadena& a, const Cadena& b) {
+    return std::strcmp(a.s_, b.s_) > 0;
+}
+
+bool operator<=(const Cadena& a, const Cadena& b) {
+    return std::strcmp(a.s_, b.s_) <= 0;
+}
+
+bool operator>=(const Cadena& a, const Cadena& b) {
+    return std::strcmp(a.s_, b.s_) >= 0;
+}
+
+bool operator!=(const Cadena& a, const Cadena& b) {
+    return std::strcmp(a.s_, b.s_) != 0;
+}
+
+//operaodres de indice
+//no const
+char& Cadena::operator[](size_t i){
+    return s_[i];
+}
+
+//const
+const char& Cadena::operator[](size_t i) const{
+    return s_[i];
+}
+
+/*-----------------METODOS------------------*/
+//funcion que devuleve el caracter del indice comprobando si este esta o no fuera de rango
+//no const
+char& Cadena::at(size_t i){
+    if (i >= length()){
+        throw std::out_of_range("Indice fuera de rango");
+    }
+    return s_[i];
+}
+
+//const
+const char& Cadena::at(size_t i) const {
+    if (i >= length()) {
+        throw std::out_of_range("Índice fuera de rango");
+    }
+    return s_[i];
+}
+
+//funcion que devuelve una cadena formada por los caracteres desde el indice i hasta los caracteres tam.
+Cadena Cadena::substr(unsigned int i, unsigned int tam) const{
+    if (i >= length() || (length() - i) < tam){
+        throw std::out_of_range("Indice fuera de rango o tamaño de la cadena supera los caracteres desde el indice");
+    }
+    char* subs = new char[tam + 1];
+    std::strncpy(subs, s_ +i, tam);
+    subs[tam] = '\0';
+    Cadena res(subs);
+    delete[] subs;
+    return res;
+}
+
--- POO/Escobar_Vidal_Mario/P0/Cadena.hpp
+++ POO/Escobar_Vidal_Mario/P0/Cadena.hpp
@@ -5,18 +5,40 @@
 
 class Cadena{
     public:
-        //constructor
-        Cadena(int n = 0, char a = '\0');
+        //constructores
+        explicit Cadena(int n = 0, char a = '\0');  //predeterminado
+        explicit Cadena(const char* cadena);        //de conversion
+        Cadena(const Cadena& otra);                 //de copia
+
+        //operador de aignación
+        Cadena& operator=(const Cadena& otra);
 
         //destructor
-        ~Cadena() { delete [] cadena_;}
+        ~Cadena();
 
         //metodos observadores
-        size_t lenght() const {return std::strlen(cadena_);}; //devuelve la longitud de la cadena c
+        size_t length() const {return std::strlen(s_);}; //devuelve la longitud de la cadena c
+
+        //operadores de comparación
+        friend bool operator== (const Cadena& a, const Cadena& b);
+        friend bool operator< (const Cadena& a, const Cadena& b);
+        friend bool operator> (const Cadena& a, const Cadena& b);
+        friend bool operator<= (const Cadena& a, const Cadena& b);
+        friend bool operator>= (const Cadena& a, const Cadena& b);
+        friend bool operator!= (const Cadena& a, const Cadena& b);
+
+        //operador de indice
+        char& operator[] (size_t i);
+        const char& operator[] (size_t i) const;
+
+        //metodos
+        char& at(size_t i);
+        const char& at(size_t i) const;
+        Cadena substr(unsigned int i, unsigned int tam) const;
     private:
         char vacia[1]{'\0'};
         size_t tam_;
-        char* cadena_{};
+        char* s_;
 
 };
 
